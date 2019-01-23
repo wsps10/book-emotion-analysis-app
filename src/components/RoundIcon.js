c6906e7b120d496f310148ea57d3f27b5d2bb2ea
gustavogimenezs@icloud.com <gustvogimenezs@icloud.com>
2019-01-22 22:04:12 -0200

    Removendo o teste de texto do botão

--- src/components/RoundIcon.js
+++ src/components/RoundIcon.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const RoundIcon = ({ bgColor="blue", color="white", family, name, title }) => {
 	return (
 		<button type="button" className="boxmodel" data-toggle="tooltip" data-placement="top" title={title}>
-		  icon
 			<FontAwesomeIcon icon={[family, name, title]}/>
 		</button>
 	);

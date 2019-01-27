247084166a10c9ff9556024d96b4c39ed413e1e6
Henrique Guerra <hfranca@outlook.com>
2019-01-27 05:59:16 -0200

    real fine tuning

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import 'tachyons';
+import './index.css';
+import './utils/styles/colors.css';
+import './utils/styles/utils.css';
+import './components/remake/a/a.css';
+
+import LoginPage from './pages/LoginPage/LoginPage';
+import SigninPage from './pages/SigninPage/SigninPage';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebookF, faGoodreadsG, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faArrowRight, faCheck, faExclamation } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArrowRight, faCheck, faExclamation, faFacebookF, faGoodreadsG, faTwitter);
+
+const App = () => {
+	return (
+		<SigninPage />
+	);
+}
+
+export default App;
---
+++ src/components/custom/RoundIcon/RoundIcon.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Icon from '../Icon/Icon';
+
+const RoundIcon = ({ 
+	classButton, 
+	classIcon, 
+	classWrapper, 
+	enabled=true, 
+	family, 
+	icon, 
+	onClick, 
+	title 
+}) => {
+	return (
+		<article className={`anima-jump dib ${classWrapper}`}>
+			<button 
+				type="button" 
+				className={`bw0 br-100 h3 outline-0 ph3 pv2 relative w3 ${classButton}` }
+				onClick={onClick} 
+				title={title}
+				disabled={!enabled}
+			>
+			<Icon 
+				family={family} 
+				icon={icon}  
+				className={`absolute dynamic-center f3 left-50 top-50 white-90 ${classIcon}`}
+			/>
+			</button>
+		</article>
+	);
+}
+
+export default RoundIcon;
